ab6bd741eea014a50120cb291da4c289fc346c19
Justin Zarko <justzk246@gmail.com>
2021-10-04 17:38:38 +0000

    Finished function 2 Part 1

--- hw-ruby-intro/lib/ruby_intro.rb
+++ hw-ruby-intro/lib/ruby_intro.rb
@@ -2,16 +2,31 @@
 
 # Part 1
 
+# Use the each method to easily add the current number to the total
+# Return the total sum
 def sum arr
   total = 0
-  
   arr.each { |currNum| total += currNum }
 
   return total
 end
 
+# Run through an array twice and pick the biggest num the first time and the second biggest the second time
+# Return the sum of the two biggest nums
 def max_2_sum arr
-  # YOUR CODE HERE
+  arrLen = arr.length
+  sum = 0
+
+  if arr.empty? == true     # if empty array, return 0
+    return sum
+  end
+  if arrLen == 1          # if array only has 1 elem, return it
+    return arr[0]
+  end
+
+  arr.sort!
+  sum = arr[-1] + arr[-2]
+  return sum
 end
 
 def sum_to_n? arr, n
